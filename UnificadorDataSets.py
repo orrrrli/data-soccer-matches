204fd01cf6166e7a53e8b328349756dce778b7e5
Orlando <castaneda.kevin@uabc.edu.mx>
2024-11-04 20:57:15 -0800

    update: merge de data sets historico de partido, estadisticas defensivas, pases y acciones de creacion de goles

--- UnificadorDataSets.py
+++ UnificadorDataSets.py
@@ -1,24 +1,22 @@
 import pandas as pd
 
-# Cargar los datos originales y el nuevo archivo de estadísticas de pases
+# Cargar los archivos de datos
 df_partidos = pd.read_csv("DataSetPartidos.txt")
-df_estadisticas_defensivas = pd.read_csv("estadisticas_defensivas.txt")
+df_estadisticas_goles = pd.read_csv("estadisticas_creacion_goles.txt")
 df_estadisticas_pases = pd.read_csv("estadisticas_pases.txt")
+df_estadisticas_defensivas = pd.read_csv("estadisticas_defensivas.txt")
 
-# Asegurar que las temporadas tengan un formato consistente
-df_partidos['Temporada'] = df_partidos['Temporada'].str.replace("2017-18", "2017-2018")
-
-# Merge con estadísticas defensivas del equipo visitante
+# Merge con estadísticas de creación de goles del equipo visitante
 merged_df = df_partidos.merge(
-    df_estadisticas_defensivas.add_suffix('_visitante'),
+    df_estadisticas_goles.add_suffix('_visitante'),
     how="left",
     left_on=["EquipoVisitante", "Temporada"],
     right_on=["Equipo_visitante", "Temporada_visitante"]
 ).drop(columns=["Equipo_visitante", "Temporada_visitante"])
 
-# Merge con estadísticas defensivas del equipo local
+# Merge con estadísticas de creación de goles del equipo local
 merged_df = merged_df.merge(
-    df_estadisticas_defensivas.add_suffix('_local'),
+    df_estadisticas_goles.add_suffix('_local'),
     how="left",
     left_on=["EquipoLocal", "Temporada"],
     right_on=["Equipo_local", "Temporada_local"]
@@ -35,6 +33,22 @@ merged_df = merged_df.merge(
 # Merge con estadísticas de pases del equipo local
 merged_df = merged_df.merge(
     df_estadisticas_pases.add_suffix('_local'),
+    how="left",
+    left_on=["EquipoLocal", "Temporada"],
+    right_on=["Equipo_local", "Temporada_local"]
+).drop(columns=["Equipo_local", "Temporada_local"])
+
+# Merge con estadísticas defensivas del equipo visitante
+merged_df = merged_df.merge(
+    df_estadisticas_defensivas.add_suffix('_visitante'),
+    how="left",
+    left_on=["EquipoVisitante", "Temporada"],
+    right_on=["Equipo_visitante", "Temporada_visitante"]
+).drop(columns=["Equipo_visitante", "Temporada_visitante"])
+
+# Merge con estadísticas defensivas del equipo local
+merged_df = merged_df.merge(
+    df_estadisticas_defensivas.add_suffix('_local'),
     how="left",
     left_on=["EquipoLocal", "Temporada"],
     right_on=["Equipo_local", "Temporada_local"]
